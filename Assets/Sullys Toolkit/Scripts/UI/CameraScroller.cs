33dfa3eb232c952544825dd862e26a3d7e83baf0
Will Smith <sullivansmith057@gmail.com>
2023-05-22 17:58:25 -0500

    Created basic UI starter Prefab, created (and debugged) camera scrolling mechanism

---
+++ Assets/Sullys Toolkit/Scripts/UI/CameraScroller.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+namespace SullysToolkit
+{
+    public class CameraScroller : MonoBehaviour
+    {
+        //Declarations
+        [Header("Scroller Settings")]
+        [SerializeField] private CinemachineVirtualCamera _UICameraReference;
+        [SerializeField] private Transform _mapOrigin;
+        [SerializeField] private Transform _cameraFollowTarget;
+        [SerializeField] [Min(0)] private float _horizontalBoundDistance;
+        [SerializeField] [Min(0)] private float _verticalBoundDistance;
+        [SerializeField] [Min(0)] private float _scrollSpeed;
+        [SerializeField] private bool _isScrollingEnabled = true;
+        [SerializeField] private bool _isDebugActive = false;
+
+        [Header("Debugging")]
+        [SerializeField] private (int, int) _scrollerMoveInput; // (xMoveInput, yMoveInput)
+        [SerializeField] private Color _gizmoColor = Color.white;
+
+
+
+        //Monobehaviours
+        private void Awake()
+        {
+            SetVirutalCameraToFollowScroller();
+        }
+
+        private void Update()
+        {
+            if (_isDebugActive)
+                ListenForDebugCommands();
+
+            if (_isScrollingEnabled)
+                MoveScrollerIfWithinBounds();
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            DrawBoundaries();
+        }
+
+
+        //Utils
+        public void SetDebug(bool newValue)
+        {
+            _isDebugActive = newValue;
+        }
+
+        public bool IsDebugActive()
+        {
+            return _isDebugActive;
+        }
+
+        public void SetHorizontalInput(int newValue)
+        {
+            _scrollerMoveInput.Item1 = Mathf.Clamp(newValue, -1, 1);
+        }
+
+        public void SetVerticalInput(int newValue)
+        {
+            _scrollerMoveInput.Item2 = Mathf.Clamp(newValue, -1, 1);
+        }
+
+        private void SetVirutalCameraToFollowScroller()
+        {
+            if (_UICameraReference != null)
+            {
+                if (_cameraFollowTarget == null)
+                    _cameraFollowTarget = this.transform;
+
+                _UICameraReference.Follow = _cameraFollowTarget;
+            }
+                
+        }
+
+        private void MoveScrollerIfWithinBounds()
+        {
+            float upperBound = _mapOrigin.position.y + _verticalBoundDistance;
+            float lowerBound = _mapOrigin.position.y - _verticalBoundDistance;
+            float rightBound = _mapOrigin.position.x + _horizontalBoundDistance;
+            float leftBound = _mapOrigin.position.x - _horizontalBoundDistance;
+
+
+            //positive up:  check if input == up and current position < upperbound
+            if (_scrollerMoveInput.Item2 > 0 && _cameraFollowTarget.position.y < upperBound)
+                MoveScrollerVertically();
+
+            //negative down
+            else if (_scrollerMoveInput.Item2 < 0 && _cameraFollowTarget.position.y > lowerBound)
+                MoveScrollerVertically();
+
+            //negative left
+            if (_scrollerMoveInput.Item1 < 0 && _cameraFollowTarget.position.x > leftBound)
+                MoveScrollerHorizontally();
+
+            //positive right
+            else if (_scrollerMoveInput.Item1 > 0 && _cameraFollowTarget.position.x < rightBound)
+                MoveScrollerHorizontally();
+
+        }
+
+        private void MoveScrollerVertically()
+        {
+            Vector2 moveDirection = new Vector2(0, _scrollerMoveInput.Item2 * _scrollSpeed * Time.deltaTime);
+            _cameraFollowTarget.transform.Translate(moveDirection);
+        }
+
+        private void MoveScrollerHorizontally()
+        {
+            Vector2 moveDirection = new Vector2(_scrollerMoveInput.Item1 * _scrollSpeed * Time.deltaTime, 0);
+            _cameraFollowTarget.transform.Translate(moveDirection);
+        }
+
+
+
+
+        //Debugging
+        private void DrawBoundaries()
+        {
+            if (_mapOrigin != null)
+            {
+                Gizmos.color = _gizmoColor;
+
+                //calculate the four corners
+                Vector2 bottomLeft = new Vector3(_mapOrigin.position.x - _horizontalBoundDistance, _mapOrigin.position.y - _verticalBoundDistance);
+                Vector2 topLeft = new Vector3(_mapOrigin.position.x - _horizontalBoundDistance, _mapOrigin.position.y + _verticalBoundDistance);
+                Vector2 bottomRight = new Vector3(_mapOrigin.position.x + _horizontalBoundDistance, _mapOrigin.position.y - _verticalBoundDistance);
+                Vector2 topRight = new Vector3(_mapOrigin.position.x + _horizontalBoundDistance, _mapOrigin.position.y + _verticalBoundDistance);
+
+                //Draw the lines
+                Gizmos.DrawLine(bottomRight, topRight); //Right side
+                Gizmos.DrawLine(bottomLeft, topLeft); //Left side
+                Gizmos.DrawLine(topLeft, topRight); //Top
+                Gizmos.DrawLine(bottomRight, bottomLeft); //Bottom
+            }
+        }
+
+        private void ListenForDebugCommands()
+        {
+            if (Input.GetKey(KeyCode.A))
+                _scrollerMoveInput.Item1 += 1;
+            else if (Input.GetKey(KeyCode.D))
+                _scrollerMoveInput.Item1 -= 1;
+            else _scrollerMoveInput.Item1 = 0;
+
+            if (Input.GetKey(KeyCode.W))
+                _scrollerMoveInput.Item2 += 1;
+            else if (Input.GetKey(KeyCode.S))
+                _scrollerMoveInput.Item2 -= 1;
+            else _scrollerMoveInput.Item2 = 0;
+
+            _scrollerMoveInput.Item1 = Mathf.Clamp(_scrollerMoveInput.Item1, -1, 1);
+            _scrollerMoveInput.Item2 = Mathf.Clamp(_scrollerMoveInput.Item2, -1, 1);
+        }
+    }
+}
+
